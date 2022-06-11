e729b880e2f0f70fb4a5a872ee17cf553623c370
bevuxyna <wannhorn@gmail.com>
2022-06-11 14:35:04 +0300

    feature: add cards

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import PopupWithForm from "./PopupWithForm";
 
 function AddPlacePopup(props) {

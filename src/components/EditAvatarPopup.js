5b1a994ecc2b664dc80393fa7f23d814b11860eb
bevuxyna <wannhorn@gmail.com>
2022-06-11 14:06:14 +0300

    upd: update avatar

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect, useRef} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import PopupWithForm from "./PopupWithForm";
 import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
